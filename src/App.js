ae6c128c8511d7c478a7f3d790d99ba5d1243b52
Saleh Ahmed Mahin <salehahmedmahin@Salehs-MacBook-Air.local>
2022-05-22 15:49:41 +0600

    tailwind css and daisyui installed and successfully config

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Banner from "./Pages/Home/Banner";
+import BusinessSummary from "./Pages/Home/BusinessSummary";
+import Parts from "./Pages/Home/Parts";
+import Reviews from "./Pages/Home/Reviews";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Banner />
+      <Parts />
+      <BusinessSummary />
+      <Reviews />
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ["./src/**/*.{html,js, jsx, ts, tsx}"],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#273ED9",
+          secondary: "#3A426D",
+          accent: "#F5F5FA",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      "cupcake",
+    ],
+  },
+  plugins: [require("daisyui")],
+};
